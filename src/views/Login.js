3ce43c52ad9af8d57c4e4d088dc9162059f7ae40
MohammedAl-Amin <mohammedamin.ababneh@gmail.com>
2022-07-06 11:53:57 +0300

    main

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,4 +1,4 @@
-import './login.css'
+
 
 
 const Login = (props) => {
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,22 +1,55 @@
+import { func } from "prop-types"
+import React, { useState } from "react"
+
+import './Signup.css'
+
+
 function SignUp(props){
+    const [formData , setFormData] = useState({
+        username: "", password: "", confirmpassword: "" 
+    })
+
+
+    function handleChange(event){
+
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    function handleSubmit(event){
+
+        event.preventDefault()
+        let document1 = document.getElementById('label1');
+        document1.classList.add(
+            'active'
+        );
+    }
+
+
     return(
         <div id="main-con">
-        <form>
-    <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" />
-    </div>
-    <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label"  style={{textTransform: "none"}}>Username</label>
+                    <input type="text" className="form-control" placeholder="Username" onChange={handleChange} name="username" value={formData.username} />
+                    <label  id="label1" className="active" >Username is already token</label>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label"  style={{textTransform: "none"}}>Password</label>
+                    <input type="password" className="form-control" placeholder="Password" onChange={handleChange} name="password"  value={formData.password} />
+                    <label id="label2" className="active">Password must be at least 8 digit long</label>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="confirmpassword" className="form-label" style={{textTransform: "none"}}>Confirm Password</label>
+                    <input type="password" className="form-control" placeholder="Confirm Password" onChange={handleChange} name="confirmpassword" value={formData.confirmpassword} />
+                    <label id="label3">Password must match</label>
+                </div>
+                <button className="btn btn-primary">Sign Up</button>
+            </form>
         </div>
     )
     }
