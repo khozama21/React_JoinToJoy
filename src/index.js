fcf4f27344677a7c40f6223eb7f0e7d0af084456
khozamaObeidat <100359224+khozamaObeidat@users.noreply.github.com>
2022-07-06 10:37:23 +0300

    catergories page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./index.css";
@@ -7,18 +7,19 @@ import Home from "./views/home";
 import Contact from "./views/Contact";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
+import { Categories } from "./views/Categories";
 
 const App = () => {
- const [hide,setHide]=useState("hide")
-  const hideHandler = ()=>{
-    if(hide=="hide"){
+  const [hide, setHide] = useState("hide")
+  const hideHandler = () => {
+    if (hide == "hide") {
       setHide("no")
-    }else{
+    } else {
       setHide("hide")
     }
 
-    
-  }  
+
+  }
   return (
     <Router>
       {/* nave bar start */}
@@ -29,44 +30,44 @@ const App = () => {
           </div>
           <div className="navLinks">
             <div className={`${hide}`}>
-            <span onClick={hideHandler} className="icon">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-            <div className="burgerLinks">
-              <li>
-                <Link className="navLink" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="navLink" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="navLink" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="navLink" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="navLink" to="/contact">
-                  contact Us
-                </Link>
-              </li>
-            </div>
+              <span onClick={hideHandler} className="icon">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+              <div className="burgerLinks">
+                <li>
+                  <Link className="navLink" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="navLink" to="/categories">
+                    Catergories
+                  </Link>
+                </li>
+                <li>
+                  <Link className="navLink" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="navLink" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="navLink" to="/contact">
+                    contact Us
+                  </Link>
+                </li>
+              </div>
             </div>
             <Link className="navLink" to="/">
               Home
             </Link>
-            <Link className="navLink" to="/">
-              Home
+            <Link className="navLink" to="/categories">
+              Catergories
             </Link>
             <Link className="navLink" to="/">
               Home
@@ -92,6 +93,7 @@ const App = () => {
       {/* body start */}
       <div>
         <Route exact component={Home} path="/" />
+        <Route exact component={Categories} path="/catergories" />
         <Route exact component={Contact} path="/contact" />
         <Route exact component={Login} path="/login" />
         <Route exact component={Signup} path="/Signup" />
