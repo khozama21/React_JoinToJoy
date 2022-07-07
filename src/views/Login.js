7407566eb5c8bcbdc590a0f06c9f8d29893f8408
Abdullah_assoli <abdullahassoli@gmail.com>
2022-07-07 12:21:48 +0200

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -3,24 +3,24 @@
 
 const Login = (props) => {
     return(
-        <div id="main-con">
-        <form>
-    <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" />
-    </div>
-    <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
-        </div>
+        <div className='create'>
+
+        <h2> Log In </h2>
+
+
+            <form  >
+                <div className="form-group">
+                    <label>Email</label>
+                    <input className='form-control' type="email" required placeholder="Email"/>
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input className='form-control' type="password" required placeholder="password" />
+                </div>
+                <button className="btn btn-primary  mt-5 mb-5 ">Submit</button>
+            
+            </form>
+</div>
     )
     }
     export default Login
