72d5baff6792e994eea2afb81681ba845bb7f6e2
Abdullah_assoli <abdullahassoli@gmail.com>
2022-07-08 17:36:19 +0200

    changes

--- src/categories/ApplicationForm.js
+++ src/categories/ApplicationForm.js
@@ -1,90 +1,129 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import { SingleCat } from './SingleCat';
-import { useHistory } from 'react-router';
-import './app.css'
-
+import { SingleCat } from "./SingleCat";
+import { useHistory } from "react-router";
+import "./app.css";
 
 export const ApplicationForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [place, setPlace] = useState("");
+  const [date, setDate] = useState("");
+  const [theme, setTheme] = useState("");
+  const [category, setCategory] = useState("");
+  const [isPneding, setIsPending] = useState(false);
+  const nav = useHistory();
 
+  const handdleSubmit = (e) => {
+    e.preventDefault();
+    const category1 = { name, email, place, date, theme, category };
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [place, setPlace] = useState("");
-    const [date, setDate] = useState("");
-    const [theme, setTheme] = useState("");
-    const [category, setCategory] = useState("");
-    const [isPneding, setIsPending] = useState(false);
-    const nav = useHistory();
+    fetch("http://localhost:8001/applications", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(category1),
+    }).then(() => {
+      console.log("Application has been successfully");
+      setIsPending(false);
+      nav.push("/categories");
+    });
+  };
+  return (
+    <div className="create   ">
+      <h2> Fill Your Application </h2>
 
-    const handdleSubmit = (e) => {
+      <form onSubmit={handdleSubmit}>
+        <div class="form-group">
+          <label>Name </label>
+          <input
+            className="form-control"
+            required
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="Name"
+          />
+        </div>
 
-        e.preventDefault();
-        const category1 = { name, email,place,date,theme,category };
+        <div class="form-group">
+          <label>Email</label>
+          <input
+            className="form-control"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+          />
+        </div>
+        <div class="form-group">
+          <label>Place of Event</label>
+          <input
+            className="form-control"
+            type="text"
+            required
+            value={place}
+            onChange={(e) => {
+              setPlace(e.target.value);
+            }}
+            placeholder="House, Farm, Hall..."
+          />
+        </div>
+        <div class="form-group">
+          <label>Date of Event</label>
+          <input
+            className="form-control"
+            type="date"
+            required
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+            placeholder="Date Of Event"
+          />
+        </div>
+        <div class="form-group">
+          <label>Theme Of Event</label>
+          <input
+            className="form-control"
+            required
+            value={theme}
+            onChange={(e) => {
+              setTheme(e.target.value);
+            }}
+            placeholder="Theme Of Event"
+          />
+        </div>
+        <label> Category </label>
+        <select
+          className="form-control"
+          required
+          type="text"
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+        >
+          <option value="Wedding">Wedding</option>
+          <option value="Conference">Conference</option>
+          <option value="Birthday">Birthday</option>
+          <option value="Graduation">Graduation</option>
+        </select>
+        {!isPneding && (
+          <button className="btn btn-primary  mt-5 mb-5 ">Submit</button>
+        )}
+        {isPneding && (
+          <button className="btn btn-primary" disabled>
+            Submiting Reservation...
+          </button>
+        )}
+      </form>
 
-
-        fetch('http://localhost:8001/applications', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(category1)
-        }).then(() => {
-            console.log('Application has been successfully')
-            setIsPending(false)
-            nav.push('/categories')
-
-        })
-
-    }
-    return (
-
-        
-<<<<<<< HEAD
-        <div className='create   '>
-=======
-        <div className='create'>
->>>>>>> 2e51cce0d2347be72ba6e53b9bd8c199a5cb2faf
-
-        <h2> Fill Your Application </h2>
-
-
-            <form onSubmit={handdleSubmit}  >
-                <div class="form-group">
-                    <label>Name </label>
-                    <input className='form-control' required type="text" value={name} onChange={e => { setName(e.target.value) }} placeholder="Name" />
-                </div>
-
-                <div class="form-group">
-                    <label>Email</label>
-                    <input className='form-control' type="email" required value={email} onChange={e => { setEmail(e.target.value) }} placeholder="Email"/>
-                </div>
-                <div class="form-group">
-                    <label>Place of Event</label>
-                    <input className='form-control' type="text" required value={place} onChange={e => { setPlace(e.target.value) }} placeholder="House, Farm, Hall..." />
-                </div>
-                <div class="form-group">
-                    <label>Date of Event</label>
-                    <input className='form-control' type="date" required value={date} onChange={e => { setDate(e.target.value) }} placeholder="Date Of Event" />
-                </div>
-                <div class="form-group">
-                    <label>Theme Of Event</label>
-                    <input className='form-control' required value={theme} onChange={e => { setTheme(e.target.value) }} placeholder="Theme Of Event" />
-                </div>
-                <label> Category </label>
-                <select className='form-control' required type="text" value={category} onChange={e => { setCategory(e.target.value) }} >
-                    <option value="Wedding">Wedding</option>
-                    <option value="Conference">Conference</option>
-                    <option value="Birthday">Birthday</option>
-                    <option value="Graduation">Graduation</option>
-
-
-
-
-                </select>
-                {!isPneding && <button className="btn btn-primary  mt-5 mb-5 ">Submit</button>}
-                {isPneding && <button className="btn btn-primary" disabled>Submiting Reservation...</button>}
-            </form>
-
-            {/* <Form onSubmit={handdleSubmit}>
+      {/* <Form onSubmit={handdleSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Name</Form.Label>
                             <Form.Control type="text" placeholder="Name" required value={name} onChange={e => { setName(e.target.value) }} />
@@ -114,12 +153,6 @@ export const ApplicationForm = () => {
 
                     </Form>
  */}
-
-
-
-
-
-
-        </div>
-    )
-}
+    </div>
+  );
+};
--- src/categories/SingleCat.js
+++ src/categories/SingleCat.js
@@ -1,83 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { useParams } from "react-router";
-import { ApplicationForm } from './ApplicationForm';
+import { ApplicationForm } from "./ApplicationForm";
 import useFetch from "./useFetch";
-import './app.css'
+import "./app.css";
 
 export const SingleCat = () => {
-<<<<<<< HEAD
   const { id } = useParams();
-  const { data: category, isPneding } = useFetch('http://localhost:8001/categories/' + id);
+  const { data: category, isPneding } = useFetch(
+    "http://localhost:8001/categories/" + id
+  );
   const nav = useHistory();
 
-=======
-    const { id } = useParams();
-    const { data: category, isPneding } = useFetch('http://localhost:8001/categories/' + id);
-    const nav = useHistory();
->>>>>>> 2e51cce0d2347be72ba6e53b9bd8c199a5cb2faf
-
-
-
-
-
-    return (
-        <div className="container-fluid text-center mt-5">
-            {isPneding && <div className="text-center"> <h3> LOADING...</h3></div>}
-<<<<<<< HEAD
-            {category && (
-                <article>
-                    <h2> {category.title} </h2>
-
-                    <div>
-
-                         {category.about}  
-                    </div>
-                </article>
-
-            )}
-            <a href="/application" className="btn btn-primary" >Fill Application</a>
+  return (
+    <div className="container-fluid text-center mt-5">
+      {isPneding && (
+        <div className="text-center">
+          {" "}
+          <h3> LOADING...</h3>
         </div>
+      )}
 
-    
-            )
-=======
-          {category && (  <article>
-                <div class="jumbotron bg-cover text-white">
-                    <div class="container py-5 text-center">
-                        <h1 class="display-4 font-weight-bold">Reservation Application</h1>
-                         
-                         
-                        {/* <a href="#" role="button" class="btn btn-primary px-5">See All Features</a> */}
-                    </div>
-                </div>
+      {category && (
+        <article>
+          <div class="jumbotron bg-cover text-white">
+            <div class="container py-5 text-center">
+              <h1 class="display-4 font-weight-bold">
+                Reservation Application
+              </h1>
 
-                <div class="container py-5">
-                    <h2 class="h3 font-weight-bold">{category.title}</h2>
-                    <div class="row">
-                        <div  >
-                            <p class="text-muted m-2">{category.about}</p>
-                            {/* <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p> */}
-                        </div>
-                        {/* <div class="col-lg-8">
+              {/* <a href="#" role="button" class="btn btn-primary px-5">See All Features</a> */}
+            </div>
+          </div>
+
+          <div class="container py-5">
+            <h2 class="h3 font-weight-bold">{category.title}</h2>
+            <div class="row">
+              <div>
+                <p class="text-muted m-2">{category.about}</p>
+                {/* <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p> */}
+              </div>
+              {/* <div class="col-lg-8">
                             <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur <strong class="font-weight-bold text-dark">adipisicing elit, sed </strong>do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <strong class="font-weight-bold text-dark">reprehenderit in voluptate </strong>velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div> */}
-                    </div>
-                </div>
+            </div>
+          </div>
+        </article>
+      )}
 
-            </article>
-)}
-
-
-
-
-
-            
-            <a href="/application" className="btn btn-primary mb-5" >Fill Application</a>
-        </div>
-
-
-    )
->>>>>>> 2e51cce0d2347be72ba6e53b9bd8c199a5cb2faf
-}
+      <a href="/application" className="btn btn-primary mb-5">
+        Fill Application
+      </a>
+    </div>
+  );
+};
