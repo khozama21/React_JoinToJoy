a6918ae2b686b73172002e149540fc34f41a1ed7
unknown <mohammedameen.ababneh@gmail.com>
2022-07-14 13:35:45 +0300

    main

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,18 +1,24 @@
 import emailjs from 'emailjs-com'
+import Footer from '../components/footer'
 const Contact = (props) => {
     function sendEmail(e){
         e.preventDefault();
         emailjs.sendForm('service_14pzppj','template_3cxyi7y',e.target ,'V5vnTPeUuLrudh_Ty');
     }
 return(
-    <div className="contactFather">
-<div className="container contact">
 
- <form id="contact-form"  onSubmit={sendEmail}>
-    <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" className="form-control" name="name" />
-    </div>
+        <>
+       <div class="home-workshops">
+            <div class="home-content-container2">
+              <div class="home-container6">
+              <div className="home-workshops">
+<div className="contactFather">
+    <div className="container contact">
+        <form id="contact-form"  onSubmit={sendEmail}>
+            <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" name="name" />
+            </div>
     <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email address</label>
         <input type="email" className="form-control" aria-describedby="emailHelp" name="email"/>
@@ -25,6 +31,16 @@ return(
 </form>
 </div>
 </div>
+</div>
+              </div>
+            </div>
+            <img alt="image" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDIwfHxidXNpbmVzc3xlbnwwfHx8fDE2NDM3MzQ3MjI&amp;ixlib=rb-1.2.1&amp;w=1500" class="home-image7" />
+          </div>
+          <Footer rootClassName="footer-root-class-name"></Footer>
+        </>
+
+           
+    
 )
 }
 export default Contact
