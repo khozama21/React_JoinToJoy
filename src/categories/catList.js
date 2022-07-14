b5bd48ab36779a6ade0162f64f18041103f8b07c
Abdullah_assoli <abdullahassoli@gmail.com>
2022-07-14 12:40:32 +0200

    Update catList.js

--- src/categories/catList.js
+++ src/categories/catList.js
@@ -19,26 +19,28 @@ const List = ({ categories }) => {
                 {/* <button className="btn btn-danger ml-5" onClick={()=>{handleDelete(blog.id)}} >Delete Blog</button> */}
 
                 <div class="speaker-card-speaker-card">
-                  <div class="speaker-card-image-container">
-                    <img
-                      alt="image"
-                      src={category.url}
-                      class="speaker-card-image"
-                    />
-                    <div class="speaker-card-read-more-container">
-                      <Link to={`/categories/${category.id}`}>
-                        <span class="speaker-card-hint">
-                          Book An Appointmnet
-                        </span>
-                      </Link>
-                      <svg viewBox="0 0 1024 1024" class="speaker-card-icon">
-                        <path d="M621.254 877.254l320-320c24.994-24.992 24.994-65.516 0-90.51l-320-320c-24.994-24.992-65.516-24.992-90.51 0-24.994 24.994-24.994 65.516 0 90.51l210.746 210.746h-613.49c-35.346 0-64 28.654-64 64s28.654 64 64 64h613.49l-210.746 210.746c-12.496 12.496-18.744 28.876-18.744 45.254s6.248 32.758 18.744 45.254c24.994 24.994 65.516 24.994 90.51 0z"></path>
-                      </svg>
+                  <Link to={`/categories/${category.id}`}>
+                    <div class="speaker-card-image-container">
+                      <img
+                        alt="image"
+                        src={category.url}
+                        class="speaker-card-image"
+                      />
+                      <div class="speaker-card-read-more-container">
+                        <Link to={`/categories/${category.id}`}>
+                          <span class="speaker-card-hint">
+                            Book An Appointmnet
+                          </span>
+                        </Link>
+                        <svg viewBox="0 0 1024 1024" class="speaker-card-icon">
+                          <path d="M621.254 877.254l320-320c24.994-24.992 24.994-65.516 0-90.51l-320-320c-24.994-24.992-65.516-24.992-90.51 0-24.994 24.994-24.994 65.516 0 90.51l210.746 210.746h-613.49c-35.346 0-64 28.654-64 64s28.654 64 64 64h613.49l-210.746 210.746c-12.496 12.496-18.744 28.876-18.744 45.254s6.248 32.758 18.744 45.254c24.994 24.994 65.516 24.994 90.51 0z"></path>
+                        </svg>
+                      </div>
                     </div>
-                  </div>
-                  <span class="speaker-card-first-name">
-                    <h4>{category.title}</h4>
-                  </span>
+                    <span class="speaker-card-first-name">
+                      <h4>{category.title}</h4>
+                    </span>
+                  </Link>
                 </div>
               </div>
             ))}
